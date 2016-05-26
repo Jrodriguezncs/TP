3dc26573c2374867e822264349d61830e51d2b09
Javier A. Rodriguez <jrodriguezncs@gmail.com>
2016-05-26 20:01:09 -0300

    Se sube archivo #2

---
+++ fizzbuzz.py
@@ -0,0 +1,16 @@
+
+"""Programa ...."""
+
+#Este programa llamado fissbuzz es un clasico de una evaluación de programación
+#El programa muestra FIZZ si es múltiplo de 3
+#El programa muestra BUZZ si es múltiplo de 5
+#El programa muestra el número si no es múltiplo de 3 o de 5
+#El programa muestra FIZZ BUZZ si es múltiplo de 3 y de 5 al mismo tiempo
+
+# Pseudocodigo:
+
+#Repito del 1 al 100, comenzando del 1. Si el nro es multiplo de 3, esto es cuando divido este nro por 3 y el resto es 0. Entonces muestro la palabra FIZZ, sino, si es multiplo de 5, o sea, al dividir ese nro por 5 el resto debe ser 0. Sino, 
+
+VARIABLE=5
+
+print("sdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
