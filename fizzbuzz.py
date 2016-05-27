9a7bbc68aa18908e0b253cac0f2c13e56d9587d0
Javier A. Rodriguez <jrodriguezncs@gmail.com>
2016-05-26 21:24:18 -0300

    Se sube codigo corresponde TK #3

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -11,6 +11,17 @@
 
 #Repito del 1 al 100, comenzando del 1. Si el nro es multiplo de 3, esto es cuando divido este nro por 3 y el resto es 0. Entonces muestro la palabra FIZZ, sino, si es multiplo de 5, o sea, al dividir ese nro por 5 el resto debe ser 0. Sino, 
 
-VARIABLE=5
 
-print("sdssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss")
+a=1
+while a<=100 :
+    
+if a%3==0 :
+    print ("FIZZ")
+    
+    if a%5==0 :
+        print ("BUZZ")
+    
+else: 
+    print a
+    
+a=a+1
